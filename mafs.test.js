3fbb999f46938bee61729baba37d01e67ff80441
Adrian-Codecov <adrian-codecov@Felipes-MacBook-Pro-5.local>
2021-04-23 20:53:24 -0700

    Preliminary tests

---
+++ mafs.test.js
@@ -0,0 +1,9 @@
+const {add, subtract} = require('./mafs');
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(add(1, 2)).toBe(3);
+});
+
+test('subtracts 4 - 1 to equal 3', () => {
+    expect(subtract(4, 1)).toBe(3);
+});
