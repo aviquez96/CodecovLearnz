485321bc52641e3dae29513e0c6c62414fa02743
Adrian-Codecov <adrian-codecov@Felipes-MacBook-Pro-5.local>
2021-04-25 16:22:14 -0700

    Added test for multiply to see test coverage update

--- mafs.test.js
+++ mafs.test.js
@@ -1,4 +1,4 @@
-const {add, subtract} = require('./mafs');
+const {add, subtract, multiply} = require('./mafs');
 
 test('adds 1 + 2 to equal 3', () => {
     expect(add(1, 2)).toBe(3);
@@ -6,4 +6,8 @@ test('adds 1 + 2 to equal 3', () => {
 
 test('subtracts 4 - 1 to equal 3', () => {
     expect(subtract(4, 1)).toBe(3);
+}); 
+
+test('multiplies 5 * 6 to equal 30', () => {
+    expect(multiply(5, 6)).toBe(30);
 }); 
